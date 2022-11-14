6aba17af91e24153c26166bb7eb326b851d40974
Vanessa Gurung <vanessagur@gmail.com>
2022-11-14 15:30:10 +1300

    sketch 2 arcs

--- creative-coding/sketches/sketch-02.js
+++ creative-coding/sketches/sketch-02.js
@@ -6,13 +6,14 @@ const settings = {
   dimensions: [1080, 1080],
 }
 
-const degToRad = (degrees) => {
-  return (degrees / 180) * Math.PI
-}
+// REPLACED WITH SKETCH UTIL FUNCTIONS:
+// const degToRad = (degrees) => {
+//   return (degrees / 180) * Math.PI
+// }
 
-const randomRange = (min, max) => {
-  return Math.random() * (max - min) + min
-}
+// const randomRange = (min, max) => {
+//   return Math.random() * (max - min) + min
+// }
 
 const sketch = () => {
   return ({ context, width, height }) => {
@@ -46,11 +47,29 @@ const sketch = () => {
       context.save()
       context.translate(x, y)
       context.rotate(-angle)
-      context.scale(random.range(1, 3), 1)
+      context.scale(random.range(0.1, 2), random.range(0.2, 0.5))
 
       context.beginPath()
-      context.rect(-w / 2, -h / 2, w, h)
+      context.rect(-w / 2, random.range(0, -h * 0.5), w, h)
       context.fill()
+      context.restore()
+
+      context.save()
+      context.translate(cx, cy)
+      context.rotate(-angle)
+
+      context.lineWidth = random.range(5, 20)
+
+      context.beginPath()
+      context.arc(
+        0,
+        0,
+        radius * random.range(0.7, 1.3),
+        slice * random.range(1, -8),
+        slice * random.range(1, 5)
+      )
+      context.stroke()
+
       context.restore()
     }
   }
