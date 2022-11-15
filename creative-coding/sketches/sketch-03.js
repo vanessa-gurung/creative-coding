add59d0c440c40aa527633aeaf4730e825d7038f
Vanessa Gurung <vanessagur@gmail.com>
2022-11-15 17:38:05 +1300

    sketch 3 animating agents

--- creative-coding/sketches/sketch-03.js
+++ creative-coding/sketches/sketch-03.js
@@ -6,10 +6,17 @@ const settings = {
   animate: true,
 }
 
+// Alternate way of animating:
+// const animate = () => {
+//   console.log('poos')
+//   requestAnimationFrame(animate)
+// }
+// animate()
+
 const sketch = ({ context, width, height }) => {
   const agents = []
 
-  for (let i = 0; i < 5000; i++) {
+  for (let i = 0; i < 10; i++) {
     const x = random.range(0, width)
     const y = random.range(0, height)
 
@@ -23,6 +30,7 @@ const sketch = ({ context, width, height }) => {
     agents.forEach((agent) => {
       agent.update()
       agent.draw(context)
+      agent.bounce(width, height)
     })
   }
 }
@@ -41,6 +49,11 @@ class Agent {
     this.pos = new Vector(x, y)
     this.vel = new Vector(random.range(-1, 1), random.range(-1, 1))
     this.radius = random.range(1, 30)
+  }
+
+  bounce(width, height) {
+    if (this.pos.x <= 0 || this.pos.x >= width) this.vel.x *= -1
+    if (this.pos.y <= 0 || this.pos.y >= height) this.vel.y *= -1
   }
 
   update() {
